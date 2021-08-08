8d59e5af0ca9deb75ef5af10b2cdbfe97201e8e2
Rupesh <rupeshsharmayo@gmail.com>
2021-08-09 00:36:38 +0530

    size

--- src/Components/CommentVideo.js
+++ src/Components/CommentVideo.js
@@ -10,7 +10,7 @@ export default function CommentVideo({ commentVideoObj }) {
             position: "relative",
         }}>
             <video
-                width="500"
+                width="400"
                 height='500'
                 autoPlay
                 loop
